79922b348cdeea515209851cec6faab9fa9f0102
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2014-12-14 13:56:51 +0200

    Added one function to the .DLL

--- DynamicLibrary/Library.cpp
+++ DynamicLibrary/Library.cpp
@@ -4,3 +4,8 @@ extern "C" __declspec(dllexport) int __stdcall doubleValue(int value)
 {
 	return value * 2;
 }
+
+extern "C" __declspec(dllexport) int __stdcall combineNumbers(int value1, int value2)
+{
+	return value1 + value2;
+}
--- DynamicLibrary/Library.h
+++ DynamicLibrary/Library.h
@@ -3,4 +3,5 @@
 extern "C"
 {
 	__declspec(dllimport) int __stdcall doubleValue(int value);
+	__declspec(dllimport) int __stdcall combineNumbers(int value1, int value2);
 }
